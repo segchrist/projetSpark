06881d6e75371ac6ba83d3cdd8f800a7a67070e0
badje <badjemm@3il.fr>
2022-02-01 23:34:11 +0100

    fill na values in a dataframe

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,4 +1,5 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, mean, round, when}
 
 object main extends App {
   val spark = SparkSession
@@ -10,7 +11,25 @@ object main extends App {
   val myData = spark.read
     .option("header", "true")
     .option("inferSchema", "true")
-    .csv("US_Accidents_Dec20_updated.csv")
+    .csv("src/data/US_Accidents_Dec20_updated.csv")
 
-  val newData = myData
+  myData.show()
+  val myDataTemp = myData.select("ID","Severity","Temperature(F)","Wind_Chill(F)")
+  myDataTemp
+    .show()
+  val dataTempMeanValue = myDataTemp.filter(col("Wind_Chill(F)").isNotNull)
+    .groupBy("Severity")
+    .agg(mean("Wind_Chill(F)").as("avg"))
+    .withColumn("avg",round(col("avg"),1))
+  dataTempMeanValue.show()
+
+  val myNewDataTemp = myDataTemp
+    .join(dataTempMeanValue,"Severity")
+    .withColumn("Wind_Chill(F)",when(col("Wind_Chill(F)").isNull,col("avg")).otherwise(col("Wind_Chill(F)")))
+    .drop("avg")
+
+  myNewDataTemp.filter(col("Wind_Chill(F)").isNull).show()
+
+
+
 }
